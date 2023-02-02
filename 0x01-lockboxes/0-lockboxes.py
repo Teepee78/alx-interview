2ffeef1108692ba686fb04f8ba631c3fa54378fe
Teepee78 <eniolatoluwalase78@gmail.com>
2023-02-03 00:55:39 +0100

    Update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -16,8 +16,10 @@ def canUnlockAll(boxes):
     """
     i = len(boxes)
 
-    if len(boxes) < 2:
+    if len(boxes) == 1:
         return True
+    if len(boxes) < 1:
+        return False
 
     # Create map
     map = {}
