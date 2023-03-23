84c76378dd4a8fbbc6fca30c06f1245337481f5f
Teepee78 <eniolatoluwalase78@gmail.com>
2023-03-23 09:56:15 +0100

    Update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,14 +13,9 @@ def makeChange(coins, total):
     count = 0
 
     for coin in sorted_coins:
-        # print(f"sum: {sum}, coin: {coin}, remainder: {total - sum}")
-        # print(count)
         while sum < total and (total - sum) >= coin:
-            # print(f"{sum} + {coin}", end=" ")
             sum += coin
-            # print(f"equal to {sum}")
             count += 1
-            # print(f"sum: {sum}")
 
     if sum == total:
         return count
